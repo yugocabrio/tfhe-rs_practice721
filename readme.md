7019af71b7109d6b788d870252c13f333b5691f1
Y5 <76672645+yugonsan@users.noreply.github.com>
2023-07-20 22:37:15 +0900

    uncomplete_version_bmi

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 - BMI calculation
 - cargo test calc_bmi -- --nocapture
+- BMIは中途半端な状態、FHEでもintegerの除算はできない
--- src/bmi.rs
+++ src/bmi.rs
@@ -6,23 +6,26 @@ fn calc_bmi() {
     let num_block = 4;
     let (client_key, server_key) = gen_keys_radix(&PARAM_MESSAGE_2_CARRY_2, num_block);
 
-    let weight: u64 = 70;  // weight in kg
-    let height: u64 = 170;  // height in cm
+    let weight: u64 = 150;  // weight in lb
+    let height: u64 = 70;  // height in in
+    let scaleFactor: u64 = 10000;
+    let bmiFactor: u64 = 703;
 
     let modulus = client_key.parameters().message_modulus.0.pow(num_block as u32) as u64;
 
     let mut ct_1 = client_key.encrypt(weight);
-    let mut ct_2 = client_key.encrypt(height);
+    let ct_2 = client_key.encrypt(height);
+    let mut ct_2_squared = ct_2.clone();  // clone for square operation
+    let mut ct_3 = client_key.encrypt(scaleFactor);
+    let mut ct_4 = client_key.encrypt(bmiFactor);
 
-    let mut ct_2_clone = ct_2.clone();
-    server_key.mul_assign_parallelized(&mut ct_2_clone, &mut ct_2);
-    ct_2 = ct_2_clone;
+    server_key.mul_assign_parallelized(&mut ct_2_squared, &ct_2);  // height^2
 
-    // The division part is not straightforward and missing in this example
-    // server_key.div_assign_parallelized(&mut ct_1, &mut ct_2);  // weight / (height^2)
+    server_key.mul_assign_parallelized(&mut ct_1, &mut ct_4);  // weight * 703
+    server_key.mul_assign_parallelized(&mut ct_1, &mut ct_3);  // weight * 703 * scaleFactor
 
     // We use the client key to decrypt the output of the circuit:
-    let output: u64 = client_key.decrypt(&ct_2);  // We only decrypt height^2
-    println!("Decrypted output: {:?}", output);
-    assert_eq!(output, (height * height) % modulus as u64);
+    let output: u64 = client_key.decrypt(&ct_1);  // We only decrypt weight * 703 * scaleFactor
+    println!("Decrypted output (weight * 703 * scaleFactor): {:?}", output);
+    assert_eq!(output, (weight * bmiFactor * scaleFactor) % modulus as u64);
 }
